1ba6fed0667767d38866cfe2f431910f29ac0352
AhmedAbdelbasetAli <ao494129@gmail.com>
2023-07-18 21:54:15 +0200

    Add APIGATEWAY

--- employee-service/src/main/java/net/AhmedAli/employeeservice/service/APIClient.java
+++ employee-service/src/main/java/net/AhmedAli/employeeservice/service/APIClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import net.AhmedAli.employeeservice.dto.DepartmentDto;
 
-@FeignClient(url = "http://localhost:8080", value = "DEPARTMENT-SERVICE")
+@FeignClient(name = "DEPARTMENT-SERVICE")
 public interface APIClient {
     @GetMapping("api/departments/{department-code}")
     DepartmentDto getDepartment(@PathVariable("department-code") String departmentcode);
